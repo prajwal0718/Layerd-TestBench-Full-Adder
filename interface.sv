9ac0cfa50ea16dbf7ae2ed7af92632bd4619bcd4
Prajwal D Nayak <65390753+prajwal0718@users.noreply.github.com>
2022-09-06 22:32:50 +0530

    Create interface.sv

---
+++ interface.sv
@@ -0,0 +1,14 @@
+//"interface.sv"
+
+//Declare all the signals in interface use logic as datatype
+interface intf();
+  
+  logic a;
+  logic b;
+  logic cin;
+  logic s;
+  logic cout;
+  
+endinterface
+
+//No clocking and modport is not present here as this is not that complex design..
